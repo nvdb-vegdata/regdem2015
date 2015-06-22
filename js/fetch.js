813a453331531a28485435e9c4686d6280ac3ebd
Benjamin Weggersen <benjamin.weggersen@gmail.com>
2015-06-22 14:51:43 +0200

    Fix indentation

--- js/fetch.js
+++ js/fetch.js
@@ -1,23 +1,23 @@
 var qwest = require('qwest');
 
 module.exports.fetch = function(callback) {
-      qwest.get('resources/objekttyper.json').then(function (responseData) {
-      var listObjects = responseData.vegObjektTyper.map( function(obj) {
-        return {value: obj.id, label:obj.navn}
-      });
+  qwest.get('resources/objekttyper.json').then(function (responseData) {
+    var listObjects = responseData.vegObjektTyper.map( function(obj) {
+      return {value: obj.id, label: obj.navn};
+    });
 
-      var sortedListObjects = listObjects.sort(function (a, b) {
-        if (a.label > b.label) {
-          return 1;
-        }
+    var sortedListObjects = listObjects.sort(function (a, b) {
+      if (a.label > b.label) {
+        return 1;
+      }
 
-        if (a.label < b.label) {
-          return -1;
-        }
+      if (a.label < b.label) {
+        return -1;
+      }
 
-        return 0;
-      });
+      return 0;
+    });
 
-      callback(sortedListObjects);
+    callback(sortedListObjects);
   });
-}
+};
