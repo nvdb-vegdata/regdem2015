bc62d41935865664a034d7da601a07f30218a26b
Kjetil Golid <kjetil golid@mac-kjetilgolid.local>
2015-06-23 14:17:00 +0200

    Add fetch function with id and bbox as parameters

--- js/fetch.js
+++ js/fetch.js
@@ -33,3 +33,15 @@ module.exports.fetchBomstasjoner = function(callback) {
     callback(responseData);
   });
 };
+
+module.exports.fetchAPIObjekter = function(objectID, box, callback) {
+  var nelat = box._northEast.lat;
+  var nelng = box._northEast.lng;
+  var swlat = box._southWest.lat;
+  var swlng = box._southWest.lng;
+  var url = 'https://www.vegvesen.no/nvdb/api/vegobjekter/' + objectID + '/.json?bbox='+ nelng + ','+ nelat +','+ swlng + ',' + swlat + '&srid=WGS84';
+  console.log(url);
+  qwest.get(url).then(function(responseData) {
+    callback(responseData);
+  });
+};
