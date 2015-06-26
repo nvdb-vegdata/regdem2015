7425d7483b397a02c9332b16be526bc35c7a3e1f
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2015-06-26 10:16:16 +0200

    Change naming of options

--- js/optiontemplate.jsx
+++ js/optiontemplate.jsx
@@ -2,15 +2,15 @@ var React = require('react');
 
 module.exports = React.createClass({
     render: function() {
-        var selected = null;
-
+        var selected = 'optionElement';
         // If this option is currently selected, render it with a green background.
         if (this.props.isSelected) {
-            selected = 'selected';
+            selected = 'optionElement selected';
         }
 
         return (
             <div className={selected}>
+                <p><i className="material-icons">room</i></p>
                 <p>{this.props.data.label}</p>
             </div>
         );
