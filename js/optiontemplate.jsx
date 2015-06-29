469cd6de4ee66cb161e68a7b0ee05e853cf2be47
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2015-06-29 13:45:54 +0200

    User input shown in Bold in options

--- js/optiontemplate.jsx
+++ js/optiontemplate.jsx
@@ -2,16 +2,23 @@ var React = require('react');
 
 module.exports = React.createClass({
     render: function() {
+        var optionData = this.props.data.label;
+        var input = this.props.userInputValue;
+
+        var pos1 = optionData.toLowerCase().indexOf(input.toLowerCase());
+        var pos2 = pos1 + input.length;
+
         var selected = 'optionElement';
-        // If this option is currently selected, render it with a green background.
         if (this.props.isSelected) {
             selected = 'optionElement selected';
         }
-
         return (
             <div className={selected}>
-                <p><i className="material-icons">room</i></p>
-                <p>{this.props.data.label}</p>
+                <p>
+                  {optionData.slice(0,pos1)}
+                  <strong>{optionData.slice(pos1,pos2)}</strong>
+                  {optionData.slice(pos2)}
+                </p>
             </div>
         );
     }
