800b86515eb791ddaf010a763a40646e42bf2eb9
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2015-07-01 12:15:27 +0200

    Intent fix

--- js/map.js
+++ js/map.js
@@ -6,28 +6,28 @@ let Kart = React.createClass({
   componentDidMount: function() {
     // Spesifisering av vegkartets projeksjon
     let crs = new L.Proj.CRS('EPSG:25833', '+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs ',
-      {
-        origin: [-2500000.0, 9045984.0],
-        resolutions: [
-          21674.7100160867,
-          10837.35500804335,
-          5418.677504021675,
-          2709.3387520108377,
-          1354.6693760054188,
-          677.3346880027094,
-          338.6673440013547,
-          169.33367200067735,
-          84.66683600033868,
-          42.33341800016934,
-          21.16670900008467,
-          10.583354500042335,
-          5.291677250021167,
-          2.6458386250105836,
-          1.3229193125052918,
-          0.6614596562526459,
-          0.33072982812632296
-        ]
-      });
+    {
+      origin: [-2500000.0, 9045984.0],
+      resolutions: [
+        21674.7100160867,
+        10837.35500804335,
+        5418.677504021675,
+        2709.3387520108377,
+        1354.6693760054188,
+        677.3346880027094,
+        338.6673440013547,
+        169.33367200067735,
+        84.66683600033868,
+        42.33341800016934,
+        21.16670900008467,
+        10.583354500042335,
+        5.291677250021167,
+        2.6458386250105836,
+        1.3229193125052918,
+        0.6614596562526459,
+        0.33072982812632296
+      ]
+    });
 
     let kartcache = 'http://m{s}.nvdbcache.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}';
 
@@ -58,9 +58,9 @@ let Kart = React.createClass({
     new L.Control.Zoom( {position: 'bottomleft'}).addTo(this.kartData);
     this.kartData.locate({setView: true, maxZoom: 15});
 
-this.kartData.on('locationfound', (e) => {
-  Marker.displayCurrentPosition(e, this.kartData);
-});
+    this.kartData.on('locationfound', (e) => {
+      Marker.displayCurrentPosition(e, this.kartData);
+    });
     this.kartData.on('moveend', () => {
       var sok = this.refs.sok;
       Marker.update(this.kartData, null, () => {
@@ -86,8 +86,8 @@ this.kartData.on('locationfound', (e) => {
   render: function() {
     return (
       <div>
-        <Sok ref="sok" updateMarkers={this.updateMarkers} />
-        <div ref="kart" className="kart"></div>
+      <Sok ref="sok" updateMarkers={this.updateMarkers} />
+      <div ref="kart" className="kart"></div>
       </div>
     );
   }
