c570c0dae53f17d63440839bdeda2f3d83bec62c
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2015-07-10 16:07:22 +0200

    Rename Geometry to geometryFields

--- js/geometryFields.jsx
+++ js/geometryFields.jsx
@@ -4,17 +4,32 @@ let RegDemActions = require('./actions.js');
 let { FlatButton } = require('material-ui');
 
 let Marker = React.createClass({
+
+  getInitialState: function () {
+    return {
+      markerPlaced: false,
+      currentMarker: null
+    };
+  },
+
   handleClick: function () {
-    RegDemActions.placeMarker();
+    RegDemActions.addGeomStart();
+    this.setMarkerPlaced(true);
+  },
+
+  setMarkerPlaced: function (value) {
+    this.setState({
+      markerPlaced: value
+    });
   },
 
   render: function () {
     return (
       <FlatButton label="Plassér Markør" onTouchTap={this.handleClick} />
-    )
+    );
   }
 });
 
 module.exports = {
   Marker: Marker
-}
+};
